b51bc02a8af9805c56f075976e778649d4e31c15
Dinith Maleesha <maleesha.live123@gmail.com>
2024-11-30 18:56:35 +0530

    arrays & object literals

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,25 @@
 "use strict";
-console.log('Hello world...');
+// arrays
+let names = ['Mario', 'Dinith'];
+let ages = [1, 2, 3, 4];
+names.push('Maleesha');
+ages.push(5);
+let fruits = ['apples', 'mango', 'Bananas'];
+fruits.push('Peaches');
+const f = fruits[2];
+let things = [1, true, 'Hello'];
+const t = things[0];
+// Object literals
+let user = {
+    firstName: 'Mario',
+    age: 21,
+    id: 1
+};
+user.firstName = 'Peach';
+// Type inderence with onject literals
+let person = {
+    name: 'Dinith',
+    score: 23,
+};
+person.name = 'Maleesha',
+    person.score = 45;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-console.log('Hello world...');
+// arrays
+
+let names: string[] = ['Mario', 'Dinith']
+let ages: number[] = [1, 2, 3, 4]
+
+names.push('Maleesha')
+ages.push(5)
+
+let fruits = ['apples', 'mango', 'Bananas']
+fruits.push('Peaches')
+
+const f = fruits[2]
+
+let things = [1, true, 'Hello']
+
+const t = things[0]
+
+
+// Object literals
+let user: {firstName: string, age: number, id: number} = {
+    firstName: 'Mario',
+    age: 21,
+    id: 1
+}
+
+user.firstName = 'Peach'
+
+
+// Type inderence with onject literals
+let person = {
+    name: 'Dinith',
+    score: 23,
+}
+
+person.name = 'Maleesha',
+person.score = 45
