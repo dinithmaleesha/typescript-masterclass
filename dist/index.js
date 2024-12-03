9215bbef194b7d5dc87d36db968f3b8ad2a8d3c9
Dinith Maleesha <maleesha.live123@gmail.com>
2024-12-03 16:15:27 +0530

    making a class

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,24 @@
 "use strict";
-console.log('hello world!!!');
+// classes 101
+class Pizza {
+    constructor(title, price) {
+        this.base = 'classic';
+        this.toppings = [];
+        this.title = title;
+        this.price = price;
+    }
+    addTopping(topping) {
+        this.toppings.push(topping);
+    }
+    removeTopping(topping) {
+        this.toppings = this.toppings.filter((t) => t !== topping);
+    }
+    selectBase(b) {
+        this.base = b;
+    }
+}
+const pizza = new Pizza('Mario Special', 15);
+pizza.addTopping('Mushrooms');
+pizza.addTopping('Olives');
+pizza.selectBase('garlic');
+console.log(pizza);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log('hello world!!!')
+// classes 101
+
+type Base = 'classic' | 'thick' | 'thin' | 'garlic'
+
+class Pizza {
+    constructor(title: string, price: number) {
+        this.title = title
+        this.price = price
+    }
+
+    title: string
+    price: number
+    base: Base = 'classic'
+    toppings: string[] = []
+
+    addTopping(topping: string): void {
+        this.toppings.push(topping)
+    }
+    removeTopping(topping: string): void {
+        this.toppings = this.toppings.filter((t) => t !== topping)
+    }
+    selectBase(b: Base): void {
+        this.base = b
+    }
+}
+
+const pizza = new Pizza('Mario Special', 15)
+
+pizza.addTopping('Mushrooms')
+pizza.addTopping('Olives')
+pizza.selectBase('garlic')
+
+console.log(pizza);
