a45ab2b05896c3fed9e6b2205628ab1763216d21
Dinith Maleesha <maleesha.live123@gmail.com>
2024-12-05 12:22:43 +0530

    generics interface

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,1 @@
 "use strict";
-// generics interface
-const collectionOne = {
-    data: ['Mario', 'Dinith', 'Peach'],
-    name: 'Mario characters'
-};
-const collectionTwo = {
-    data: [10, 39, 20, 40, 37],
-    name: 'Winning lottery numbers'
-};
-function randomCollectionItem(c) {
-    const i = Math.floor(Math.random() * c.data.length);
-    return c.data[i];
-}
-const resultOne = randomCollectionItem(collectionOne);
-console.log(resultOne);
