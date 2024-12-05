cd97dd8b05b2b4f5be2dde2a982d7d653acf0a40
Dinith Maleesha <maleesha.live123@gmail.com>
2024-12-05 12:12:07 +0530

    intersection Types

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,1 @@
 "use strict";
-// Intersection type
-function addIdToValue(val) {
-    const id = Math.random();
-    return Object.assign(Object.assign({}, val), { id });
-}
-const post = addIdToValue({ title: 'Title', thumbsUp: 34 });
-console.log(post);
