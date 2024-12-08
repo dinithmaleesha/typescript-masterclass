e9d37e70f581cfeac22a668703de8702cdb329f6
Dinith Maleesha <maleesha.live123@gmail.com>
2024-12-08 17:00:29 +0530

    Components & Props

---
+++ ninja-blog/src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <main>
+      <h2>About</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio incidunt perspiciatis porro. Ut, facere maiores? Accusantium ea veritatis nemo! Omnis at fugiat ex modi accusantium nobis, inventore unde totam alias?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet et quisquam odit. Repellat, id aperiam numquam esse odit dolor quos sequi dicta accusamus excepturi omnis maiores sunt dignissimos qui, cum natus placeat. Commodi fugit, exercitationem voluptate officia, eius ab doloremque nam mollitia adipisci praesentium explicabo repellat omnis quia aliquam a unde iusto nostrum? Ad consectetur possimus, voluptatibus accusamus aliquid sed obcaecati voluptatem recusandae vel magni. Recusandae quisquam cum vel dolorem, nobis accusantium vero similique unde, nisi corporis vitae! Quae quas ipsa vero harum eligendi enim officia magnam assumenda, modi deleniti quo veritatis corporis alias at minima odit recusandae! Ipsa, minima.</p>
+    </main>
+  )
+}
