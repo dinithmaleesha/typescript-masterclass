292d5a7d2dfb2341b8e5113674a09fa2a0b4d172
Dinith Maleesha <maleesha.live123@gmail.com>
2024-12-09 12:07:55 +0530

    done

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+// enum
+
+enum Priority {
+    Lowest = 0,
+    Low = 1,
+    Medium = 2,
+    High = 3,
+    Urgent = 4
+}
+
+function addTicket(details: string, priority: Priority) {
+    if (priority === Priority.Low) {}
+    if (priority === 1) {}
+    if (priority === 2) {}
+    if (priority === 3) {}
+    if (priority === 4) {}
+}
+
+addTicket('fix', Priority.Urgent)
